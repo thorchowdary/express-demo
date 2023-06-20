61e3e9007de6117dabd5c4dadf67b9ca0f18dfd3
KETINENI TARUN <44497360+thorchowdary@users.noreply.github.com>
2023-06-20 09:13:35 +0530

    Update express-demo.sh

--- express-demo.sh
+++ express-demo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-sudo yum install -y nodejs
 npm i express
 npm i -g nodemon
 zip -r express-demo.zip *
-aws s3 cp express-demo.zip s3://bucketName
+aws s3 cp express-demo.zip s3://dev-backendd
